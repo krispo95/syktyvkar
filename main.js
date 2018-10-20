2e0c19ebb0c3bc8275c4c977a81c36cc7660c0b1
Phlogistonny <pogosyan.chris@gmail.com>
2018-10-20 19:01:13 +0300

    added switch between pages , added form
    added 2 buttons (add and list)

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Привет, Vue!'
-    }
-  })
-
   var menu = new Vue({
     el: '#menu',
     data: {
@@ -27,3 +20,58 @@ var app = new Vue({
       ]
     }
   })
+
+  var menu = new Vue({
+    el: '#app',
+    data: {
+        page:'list',
+        mode: 'add',
+        items: [
+            {name:"Weatcher", id:0},
+            {name:"Wither", id:1},
+            {name:"Fire", id:2},
+        ],
+        idCounter: 3,
+        name: "",
+        checkbox: "",
+        description: "",
+        label: "",
+        color: "",
+    },
+    methods: {
+        add: function() {
+            this.page = 'form';  
+            this.mode = 'add';
+            this.message="form";
+        },
+        list: function() {
+            this.page= 'list';  
+            this.message = "list";
+        },
+        addItem: function(){
+        console.log("ok");
+        this.idCounter++;
+         this.page="list";
+         this.mode="add"
+        this.items.push({
+          name: this.name,
+          checkbox: this.checkbox,
+          description: this.description,
+          label: this.label,
+          color: this.color,
+          id: this.idConuter,
+        })        
+        },
+        editItem:function(){
+          this.page="list";
+         this.mode="add";
+        },
+        selectItem: function(itemId){
+          this.page="form";
+          this.mode="edit";
+          console.log(itemId);
+
+        }
+},
+
+})
