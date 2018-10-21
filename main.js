487148e4dd3a6bea3aff71162e997c16d58a7786
Phlogistonny <pogosyan.chris@gmail.com>
2018-10-21 21:36:40 +0300

    add workaround for delete button

--- main.js
+++ main.js
@@ -1,97 +1,107 @@
-  var menu = new Vue({
-    el: '#menu',
-    data: {
-      todos: [
-        { text: 'Новости' },
-        { text: 'Свободные страницы' },
-        { text: 'Афиша' },
-        { text: 'Гости и эксперты' },
-        { text: 'Красота и здоровье' },
-        { text: 'Спецтемы' },
-        { text: 'Опросы' },
-        { text: 'Спавочники' },
-        { text: 'Вакансии' },
-        { text: 'Конкурсы' },
-        { text: 'Пользователи' },
-        { text: 'Фишки' },
-        { text: 'Меню сайта' },
-        { text: 'Актуальная информация' },
-        { text: 'Настройки' }
-      ]
-    }
-  })
+var menu = new Vue({
+  el: '#menu',
+  data: {
+    todos: [
+      { text: 'Новости' },
+      { text: 'Свободные страницы' },
+      { text: 'Афиша' },
+      { text: 'Гости и эксперты' },
+      { text: 'Красота и здоровье' },
+      { text: 'Спецтемы' },
+      { text: 'Опросы' },
+      { text: 'Спавочники' },
+      { text: 'Вакансии' },
+      { text: 'Конкурсы' },
+      { text: 'Пользователи' },
+      { text: 'Фишки' },
+      { text: 'Меню сайта' },
+      { text: 'Актуальная информация' },
+      { text: 'Настройки' }
+    ]
+  }
+})
 
-  var menu = new Vue({
-    el: '#app',
-    data: {
-        page:'list',
-        mode: 'add',
-        items: {
-          "Weatcher" : {name:"Weatcher"},
-          "Wither" : {name:"Wither"},
-          "Fire" : {name:"Fire"},
-       },
-        formName: "",
-        formCheckbox: "",
-        formDescription: "",
-        formLabel: "",
-        formColor: "",
+var menu = new Vue({
+  el: '#app',
+  data: {
+    page: 'list',
+    mode: 'add',
+    items: {
+      "First": {
+        name: "First",
+        checkbox: true,
+        description: 'First description',
+        label: 'First label',
+        color: '#111111',
+      },
+      "Second": {
+        name: "Second",
+        checkbox: false,
+        description: 'Second description',
+        label: 'Second label',
+        color: '#222222',
+      },
     },
-    methods: {
-        add: function() {
-            this.page = 'form';  
-            this.mode = 'add';
-            this.message="form";
-            this.formName = '';
-            this.formCheckbox = '';
-            this.formDescription = '';
-            this.formLabel = '';
-            this.formColor = '';
-        },
-        list: function() {
-            this.page= 'list';  
-            this.message = "list";
-        },
-        addItem: function(){
-        console.log("ok");
-         this.page="list";
-         this.mode="add";
-         this.items[this.formName] = {
-          name: this.formName,
-          checkbox: this.formCheckbox,
-          description: this.formDescription,
-          label: this.formLabel,
-          color: this.formColor,
-         };
+    formName: "",
+    formCheckbox: "",
+    formDescription: "",
+    formLabel: "",
+    formColor: "",
+  },
+  methods: {
+    add: function () {
+      this.page = 'form';
+      this.mode = 'add';
+      this.message = "form";
+      this.formName = '';
+      this.formCheckbox = '';
+      this.formDescription = '';
+      this.formLabel = '';
+      this.formColor = '';
+    },
+    list: function () {
+      this.page = 'list';
+      this.message = "list";
+    },
+    addItem: function () {
+      this.page = "list";
+      this.items[this.formName] = {
+        name: this.formName,
+        checkbox: this.formCheckbox,
+        description: this.formDescription,
+        label: this.formLabel,
+        color: this.formColor,
+      };
 
-        },
-        editItem:function(){
-          this.page="list";
-         this.mode="add";
-         this.items[this.formName] = {
-          checkbox: this.formCheckbox,
-          description: this.formDescription,
-          label: this.formLabel,
-          color: this.formColor,
-         };
-        },
-        selectItem: function(itemName){
-          this.page="form";
-          this.mode="edit";
-          var item = this.items[itemName];
-          console.log(item);
-          console.log(item.color);
-          this.formName = item.name;
-          this.formCheckbox = item.checkbox;
-          this.formDescription = item.description;
-          this.formLabel = item.label;
-          this.formColor = item.color;
-        },
-        deleteItem: function(itemName){
-          this.page="list";
-          delete this.items[itemName];
-        }
-},
+    },
+    editItem: function () {
+      this.page = "list";
+      this.items[this.formName] = {
+        checkbox: this.formCheckbox,
+        description: this.formDescription,
+        label: this.formLabel,
+        color: this.formColor,
+      };
+    },
+    selectItem: function (itemName) {
+      this.page = "form";
+      this.mode = "edit";
+      var item = this.items[itemName];
+      console.log(item);
+      console.log(item.color);
+      this.formName = item.name;
+      this.formCheckbox = item.checkbox;
+      this.formDescription = item.description;
+      this.formLabel = item.label;
+      this.formColor = item.color;
+    },
+    deleteItem: function (itemName) {
+      this.page = 'form'; //this is workaround to forse rendering list
+      this.page = "list";
+      delete this.items[itemName];
+      this.items = this.items;
+    }
+  },
 
 
 })
